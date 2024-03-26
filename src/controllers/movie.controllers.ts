3b8c00cef61aa2b547ee1cdc3e776df184975821
Victor Jauregui <vicjaudom@hotmail.com>
2024-03-26 12:37:51 +0100

    feat(prisma): migrate from mongoose to prisma

--- src/controllers/movie.controllers.ts
+++ src/controllers/movie.controllers.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from "express";
 import MovieModel from "../models/movie.model";
 import UserModel from "../models/user.model";
+import prisma from "../db/client";
 
 export const createMovie = async (req: Request, res: Response) => {
   const { name, image } = req.body;
   const { userId } = req.params;
   try {
-    const movie = await MovieModel.create({ name, image });
-    await UserModel.findByIdAndUpdate(
-        { _id: userId }, 
-        { $push: {movies: movie._id}});
-        
+    const movie = await prisma.movies.create({
+      data:{ name, image, user: { connect : {id:userId}} }
+    });     
     res.status(201).send(movie);
   } catch (error) {
     res.status(400).send(error);
@@ -18,7 +17,7 @@ export const createMovie = async (req: Request, res: Response) => {
 };
 export const getAllMovies = async (req: Request, res: Response) => {
   try {
-    const allMovies = await MovieModel.find();
+    const allMovies = await prisma.movies.findMany();
     res.status(201).send(allMovies);
   } catch (error) {
     res.status(400).send(error);
--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
 import UserModel from "../models/user.model";
+import prisma from "../db/client";
 
 export const getAllUser = async (req: Request, res: Response) => {
   try {
-    const allUsers = await UserModel.find().populate("movies");
+    const allUsers = await prisma.user.findMany({
+      include:{
+        movies: true
+      }
+    });
     res.status(200).send(allUsers);
   } catch (error) {
     res.status(400).send(error);
@@ -14,7 +19,9 @@ export const createUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
 
   try {
-    const newUser = await UserModel.create({ name, email, password });
+    const newUser = await prisma.user.create({
+      data:{ name, email, password }
+    });
     res.status(201).send(newUser);
   } catch (error) {
     res.status(400).send(error);
@@ -26,11 +33,10 @@ export const updateUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
 
   try {
-    const userUpdated = await UserModel.findByIdAndUpdate(
-      { _id: userId },
-      { name, email, password },
-      { new: true }
-    )
+    const userUpdated = await prisma.user.update({
+      where: {id:userId},
+      data:{name, email, password}
+    })
     res.status(201).send(userUpdated)
   } catch (error) {
     res.status(400).send(error)
@@ -41,7 +47,9 @@ export const updateUser = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
   try {
-    const userDeleted = await UserModel.findByIdAndDelete( { _id: userId})
+    const userDeleted = await prisma.user.delete({ 
+     where: { id: userId}
+    })
     res.status(200).send(userDeleted)
   } catch (error) {
     res.status(400).send(error)
